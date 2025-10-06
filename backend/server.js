6414d9237f27780d4e17bc7462d2dca12b4a13d2
Kurnia Ramadhan <kramadhan@gmail.com>
2025-10-06 10:20:27 +0700

    Update package.json to include devDependencies and modify server.js for ES module syntax and new endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/products", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
 });
