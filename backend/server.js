847aa7ab99993199c3155831b3cec91fc2e0986c
Kurnia Ramadhan <kramadhan@gmail.com>
2025-10-09 10:13:46 +0700

    update code

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
-// dotenv.config(); 
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
